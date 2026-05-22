e51e8a58ebf3958edb837030407d2eb9cfb8251c
Vaibhav Kadwey <vaibhav3kadwey@gmail.com>
2026-05-23 00:57:16 +0530

    added code for event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,8 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+/** Events will store all the events that hits the event bus */
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
@@ -23,6 +28,11 @@ app.post("/events", (req, res) => {
   });
 
   res.send({ status: "OK" });
+});
+
+/** End point to access all the events */
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
